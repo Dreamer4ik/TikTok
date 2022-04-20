41a1221705e2fd74ebfc6abe8cfc3c1293878424
Ivan Potapenko <dreamer4ik@gmail.com>
2022-04-20 19:55:40 +0300

    Initial Commit

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Ivan Potapenko on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
