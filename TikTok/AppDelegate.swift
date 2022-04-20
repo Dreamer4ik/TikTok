6e6f4af617ea248099542c08cdc51e95b05427ab
Ivan Potapenko <dreamer4ik@gmail.com>
2022-04-20 20:14:05 +0300

    Setting up project

--- TikTok/AppDelegate.swift
+++ TikTok/AppDelegate.swift
@@ -9,11 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
