f3655ed4529d9fcd5e2063c569c3bcaa276c2618
Ivan Potapenko <dreamer4ik@gmail.com>
2022-05-07 18:11:14 +0300

    Sign Out

--- TikTok/Controllers/Other/SettingsViewController.swift
+++ TikTok/Controllers/Other/SettingsViewController.swift
@@ -8,14 +8,88 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Settings"
         view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        createFooter()
     }
     
+    func createFooter() {
+        let footer = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: 100))
+        let button = UIButton(frame: CGRect(x: (view.width-200)/2, y: 25, width: 200, height: 50))
+        button.setTitle("Sign Out", for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
+        button.addTarget(self, action: #selector(didTapSignOut), for: .touchUpInside)
+        footer.addSubview(button)
+        tableView.tableFooterView = footer
+    }
+    
+    @objc private func didTapSignOut() {
+        let actionSheet = UIAlertController(title: "Sign Out",
+                                            message: "Would you like to sign out?",
+                                            preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { [weak self] _ in
+            AuthManager.shared.signOut { success in
+                DispatchQueue.main.async {
+                    if success {
+                        UserDefaults.standard.setValue(nil, forKey: "username")
+                        UserDefaults.standard.setValue(nil, forKey: "profile_picture_url")
+                        
+                        let vc = SignInViewController()
+                        let navVC = UINavigationController(rootViewController: vc)
+                        navVC.modalPresentationStyle = .fullScreen
+                        self?.present(navVC, animated: true)
+                        self?.navigationController?.popToRootViewController(animated: true)
+                        self?.tabBarController?.selectedIndex = 0
+                    }
+                    else {
+                        let alert = UIAlertController(title: "Woops",
+                                                            message: "Something went wrong when signing out. Please try again.",
+                                                      preferredStyle: .alert)
+                        actionSheet.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                        self?.present(alert, animated: true)
+                    }
+                }
+            }
+        }))
+        
+        present(actionSheet, animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    
+}
 
-  
-
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+                                                 for: indexPath)
+        
+        cell.textLabel?.text = "TikTok"
+        return cell
+    }
+    
+    
 }
